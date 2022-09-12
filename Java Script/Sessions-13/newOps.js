25620d8471ac7ccf2c877f3e92181fa23f39a995
muhammedvuslat <v.sltcevikev@gmail.com>
2022-09-12 17:27:06 +0300

    object_copy

--- Java Script/Sessions-13/newOps.js
+++ Java Script/Sessions-13/newOps.js
@@ -186,3 +186,18 @@ hisNumbers.push(101);
 console.log("MY:", myNumbers, "HIS:", hisNumbers);
 
 console.log(herNumbers);
+
+//* Object Copy
+
+const myObj = { a: 1, b: 2, c: 4 };
+const herObj = { a: 2, z: 4, c: 3 };
+
+const copiedObj = { ...myObj };
+console.log(copiedObj);
+
+copiedObj.c = "33";
+console.log(copiedObj, myObj);
+
+const combinedObj = { ...herObj, ...myObj };
+
+console.log(combinedObj);
