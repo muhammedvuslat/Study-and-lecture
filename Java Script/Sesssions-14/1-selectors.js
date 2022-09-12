2d5b9d50a90fc3a9492cd8478c94a71291629041
muhammedvuslat <v.sltcevikev@gmail.com>
2022-09-12 20:48:36 +0300

    js-Doom_2

--- Java Script/Sesssions-14/1-selectors.js
+++ Java Script/Sesssions-14/1-selectors.js
@@ -6,3 +6,36 @@ console.log("***** SELECTORS *****");
 
 //* EXAMPLE-1
 //*-------------------------------------------
+const h1 = document.getElementById("header");
+console.log(h1);
+h1.style.color = "white";
+h1.style.backgroundColor = "red";
+
+//* EXAMPLE-2
+//*-------------------------------------------
+const myInput = document.getElementById("input");
+const button = document.getElementById("btn");
+button.style.backgroundColor = "black";
+button.style.color = "magenta";
+button.style.width = "7rem";
+button.style.border = "none";
+button.style.borderRadius = "10px";
+button.style.padding = "1rem";
+button.style.fontSize = "1.1rem";
+myInput.style.padding = "1rem";
+
+//*===========================================
+//*          GETELEMENTSBYTAGNAME()
+//*===========================================
+
+//*EXAMPLE-3
+//*-------------------------------------------
+
+//? HTML kodlarini calistirir. (Guvenlik acisindan problemli)
+
+//*===========================================
+//*          GETELEMENTSBYCLASSNAME()
+//*===========================================
+
+//* EXAMPLE-4
+//* ------------------------------------------
