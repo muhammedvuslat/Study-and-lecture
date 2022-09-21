5c70c4d6a15015ac4f84c60353fb358ddb34282a
muhammedvuslat <v.sltcevikev@gmail.com>
2022-09-21 14:03:10 +0300

    js-dom-2

--- Java Script/Sessions-17/app.js
+++ Java Script/Sessions-17/app.js
@@ -11,6 +11,7 @@ addBtn.addEventListener("click", () => {
       completed: false,
       text: todoInput.value,
     };
+    //! yeni bir li elementi olusturup bunu DOM'a bas
     createListElement(newTodo);
   }
 });
@@ -19,5 +20,20 @@ const createListElement = (newTodo) => {
   const li = document.createElement("li");
   li.setAttribute("id", newTodo.id); //! alternatif olarak li.id = newTodo.id [id ismi ve newTodo.id Valuesi]
   //! id="1663754543101" gibi
-  console.log(li);
+  const okIcon = document.createElement("i");
+  okIcon.setAttribute("class", "fas fa-check");
+  li.appendChild(okIcon);
+
+  //? todo basligi icin bir p elementi ve yazi dugumu olusturarak li'ye bagla
+  const p = document.createElement("p");
+  const pTextNode = document.createTextNode(newTodo.text);
+  p.appendChild(pTextNode), li.appendChild(p);
+
+  //? delete ikonu olustur ve li elementine bagla
+
+  const deleteIcon = document.createElement("i");
+  deleteIcon.setAttribute("class", "fas fa-trash");
+  li.appendChild(deleteIcon);
+
+  todoUl.appendChild(li);
 };
