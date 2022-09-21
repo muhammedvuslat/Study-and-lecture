95032b4ec40edf93407acee4d3383032dda3a6e9
muhammedvuslat <v.sltcevikev@gmail.com>
2022-09-21 14:27:43 +0300

    js-dom-3

--- Java Script/Sessions-17/app.js
+++ Java Script/Sessions-17/app.js
@@ -13,20 +13,26 @@ addBtn.addEventListener("click", () => {
     };
     //! yeni bir li elementi olusturup bunu DOM'a bas
     createListElement(newTodo);
+    todoInput.value = "";
   }
 });
 
 const createListElement = (newTodo) => {
+  const { id, completed, text } = newTodo;
   const li = document.createElement("li");
-  li.setAttribute("id", newTodo.id); //! alternatif olarak li.id = newTodo.id [id ismi ve newTodo.id Valuesi]
+  li.setAttribute("id", id); //! alternatif olarak li.id = newTodo.id [id ismi ve newTodo.id Valuesi]
   //! id="1663754543101" gibi
+
+  completed ? li.classList.add("checked") : "";
+
+  //! Tik ikonun oluştur ve oluşturulanı li elemntine bağla
   const okIcon = document.createElement("i");
   okIcon.setAttribute("class", "fas fa-check");
   li.appendChild(okIcon);
 
   //? todo basligi icin bir p elementi ve yazi dugumu olusturarak li'ye bagla
   const p = document.createElement("p");
-  const pTextNode = document.createTextNode(newTodo.text);
+  const pTextNode = document.createTextNode(text);
   p.appendChild(pTextNode), li.appendChild(p);
 
   //? delete ikonu olustur ve li elementine bagla
@@ -34,6 +40,16 @@ const createListElement = (newTodo) => {
   const deleteIcon = document.createElement("i");
   deleteIcon.setAttribute("class", "fas fa-trash");
   li.appendChild(deleteIcon);
-
+  //? Oluşan  li elementini ul'ye child olarak ata
   todoUl.appendChild(li);
+};
+//! Enter tuş eklemesi
+todoInput.addEventListener("keydown", (e) => {
+  if (e.code === "Enter") {
+    addBtn.click();
+  }
+});
+//! Başlangıç focusu
+window.onload = function () {
+  todoInput.focus();
 };
