6843826e3660e0eddc7e7f9ead1e3481ee9710a7
muhammedvuslat <v.sltcevikev@gmail.com>
2022-09-27 06:59:59 +0300

    minus&plus-button

--- Java Script/Sessions-20/app.js
+++ Java Script/Sessions-20/app.js
@@ -16,13 +16,21 @@ const productsDiv = document.querySelector(".products");
 productsDiv.addEventListener("click", (event) => {
   if (event.target.className == "fa-solid fa-minus") {
     //*! class name tanımlamasında seçilecek butonda clasın tam ismi alınması gerekli */
-    console.log("clicked the minus button!");
+    /*console.log("clicked the minus button!"); //*? Clg ile konsolda kontrolünü sağladık  */
+    if (event.target.parentElement.querySelector(".quantity").innerText > 1) {
+      event.target.parentElement.querySelector(".quantity").innerText--;
+    } else {
+      if (confirm("Product will be removed!?")) {
+        // product remove
+      }
+    }
   } else if (event.target.classList.contains("fa-plus")) {
     //*! classList tanımlamasında seçilecek butonun clasında bulunan her hangi isim yazılabilinir fakat contain ile var olup olmadığını çek eder tru ise fonk. çalışır
-    console.log("clicked the plus button!");
+    /*console.log("clicked the plus button!"); //*? Clg ile konsolda kontrolünü sağladık  */
+    event.target.previousElementSibling.innerText++;
   } else if (event.target.classList.contains("remove-product")) {
     console.log("clicked the remove button!");
   } else {
-    console.log("clicked other elements!");
+    /*console.log("clicked other elements!");//*? Clg ile konsolda kontrolünü sağladık  */
   }
 });
