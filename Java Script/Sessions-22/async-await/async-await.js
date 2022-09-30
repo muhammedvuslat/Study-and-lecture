af23f5e996dd6af1527f90c4bf99fb6ac0ca5eeb
muhammedvuslat <v.sltcevikev@gmail.com>
2022-09-30 07:43:31 +0300

    async-await-1

---
+++ Java Script/Sessions-22/async-await/async-await.js
@@ -0,0 +1,28 @@
+//*=================================================
+//*               ASYNC-AWAIT
+//*=================================================
+//? Async-Await ECMAScript 2017 ile Javascript diline eklenmistir.
+//? Aslinda Promise yapisinin syntax olarak basitlestirilmis halidir.
+//? Bu baglamda sentetik seker benzetmesi yapilabilir.
+
+//* Bir fonskiyonu asenkron hale getirmek icin fonksiyon keyword'nun onune
+//* async keyword'u eklenir.
+
+//* Bir async fonksiyon icerisinde await keyword'u ile yapilan istegin cevabinin
+//* beklenmesi saglanir.
+
+//* Aslinda dizilis olarak senkron mantiga benzeyen kod yazarak Asenkron
+//* kod yazmayı mumkun kilar.
+
+//* Await, promise-temelli herhangi bir fonksiyonun onune getirilerek getirildigi
+//* satirdaki kodun durudurulmasini saglar. Yapilan istek yerine getirilip sonuc
+//* degerlerinin dondurulmesine ile kodun calismasi devam eder.
+
+const getNews = async function () {
+  const API_KEY = "93037a64725447a1a8375d55484710e9";
+  const url =
+    "https://newsapi.org/v2/top-headlines?country=us&apiKey=" + API_KEY;
+  const res = await fetch(url);
+  console.log(res);
+};
+getNews();
