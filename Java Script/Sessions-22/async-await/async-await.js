46f317748d710cecbac0db9cd6609a700e342f60
muhammedvuslat <v.sltcevikev@gmail.com>
2022-10-03 06:54:26 +0300

    async-await-3

--- Java Script/Sessions-22/async-await/async-await.js
+++ Java Script/Sessions-22/async-await/async-await.js
@@ -18,14 +18,17 @@
 //* satirdaki kodun durudurulmasini saglar. Yapilan istek yerine getirilip sonuc
 //* degerlerinin dondurulmesine ile kodun calismasi devam eder.
 
+let isError = false;
 const getNews = async function () {
   const API_KEY = "93037a64725447a1a8375d55484710e9"; //! 9
   const url =
     "https://newsapi.org/v2/top-headlines?country=us&apiKey=" + API_KEY;
+
   try {
     const res = await fetch(url);
     if (!res.ok) {
-      throw new Error(`Something went wrong: ${res.status}`);
+      isError = true;
+      // throw new Error(`Something went wrong: ${res.status}`);
     }
     const data = await res.json(); //! json formatı ile içerisine erişim
     // console.log(data.articles); //! articles içierisini görmek için data.articles
@@ -36,6 +39,13 @@ const getNews = async function () {
 };
 const renderNews = (news) => {
   const newsList = document.getElementById("news-list");
+  if (isError) {
+    newsList.innerHTML += `
+    <h2>News can not be fetched </h2>
+    <img src="../img/404.png" alt="">
+    `;
+    return;
+  }
   news.forEach((item) => {
     const { title, description, urlToImage, url } = item; //! Destructuring yapı
     newsList.innerHTML += `
