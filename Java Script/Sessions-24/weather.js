fbe0eced06cbf0778d933e7113788ac9ef62cd3b
muhammedvuslat <v.sltcevikev@gmail.com>
2022-10-04 08:00:20 +0300

    weather-3

---
+++ Java Script/Sessions-24/weather.js
@@ -0,0 +1,9 @@
+const form = document.queryCommandIndeterm("section.top-banner form");
+const input = document.querySelector(".container input");
+const msg = document.querySelector("span.msg");
+const list = document.querySelector(".ajax-section .cities");
+
+localStorage.setItem(
+  "tokenKey",
+  "APmZLfxtMtsZU2trsJj32PEnVbHzYqNQLpKk76K4PGiZ4xFV2VieIJjIg79JYHA4"
+);
