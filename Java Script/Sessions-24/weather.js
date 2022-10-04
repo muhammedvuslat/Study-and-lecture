5f4f2a4f838191534d5cd211c7ff3b3006cca6f6
muhammedvuslat <v.sltcevikev@gmail.com>
2022-10-04 16:10:21 +0300

    Weather-city-search-2

--- Java Script/Sessions-24/weather.js
+++ Java Script/Sessions-24/weather.js
@@ -21,18 +21,30 @@ const getWeatherDataFromApi = async () => {
   const units = "metric";
   const lang = "tr";
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&appid=${tokenKey}&units=${units}&lang=${lang}`;
-  const response = await fetch(url).then((response) => response.json());
-  console.log(response);
-  const { main, sys, weather, name } = response;
-  const iconUrl = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${weather[0].icon}.svg`;
-  const cityNameSpans = list.querySelectorAll(".city span");
-  const cityNameSpansArray = Array.from(cityNameSpans);
-  if (cityNameSpansArray.length > 0) {
-  }
-  console.log(cityNameSpansArray);
-  const createdLi = document.createElement("li");
-  createdLi.classList.add("city");
-  createdLi.innerHTML = `
+  try {
+    const response = await fetch(url).then((response) => response.json());
+    console.log(response);
+    const { main, sys, weather, name } = response;
+    const iconUrl = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${weather[0].icon}.svg`;
+    const cityNameSpans = list.querySelectorAll(".city span");
+    const cityNameSpansArray = Array.from(cityNameSpans);
+    if (cityNameSpansArray.length > 0) {
+      const filteredArray = cityNameSpansArray.filter(
+        (span) => span.innerText == name
+      );
+      if (filteredArray.length > 0) {
+        msg.innerText = `You already know the weather for ${name} please search for another city `;
+        setTimeout(() => {
+          msg.innerText = "";
+        }, 4000);
+        form.reset();
+        return;
+      }
+    }
+    console.log(cityNameSpansArray);
+    const createdLi = document.createElement("li");
+    createdLi.classList.add("city");
+    createdLi.innerHTML = `
   <h2 class="city-name" data-name="${name}, ${sys.country}">
                              <span>${name}</span>
                              <sup>${sys.country}</sup>
@@ -46,7 +58,14 @@ const getWeatherDataFromApi = async () => {
                                   weather[0].description
                                 }</figcaption>
                             </figure>`;
-  //! append vs prepend
-  list.prepend(createdLi);
+    //! append vs prepend
+    list.prepend(createdLi);
+    form.reset();
+  } catch (error) {
+    msg.innerText = `${input.value} Not Found  `;
+    setTimeout(() => {
+      msg.innerText = "";
+    }, 4000);
+  }
   form.reset();
 };
