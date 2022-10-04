cb7800cf1e734f653b40ac263f5ddf84830033f5
muhammedvuslat <v.sltcevikev@gmail.com>
2022-10-04 08:55:40 +0300

    API-1

--- Java Script/Sessions-24/weather.js
+++ Java Script/Sessions-24/weather.js
@@ -1,4 +1,4 @@
-const form = document.queryCommandIndeterm("section.top-banner form");
+const form = document.querySelector("section.top-banner form");
 const input = document.querySelector(".container input");
 const msg = document.querySelector("span.msg");
 const list = document.querySelector(".ajax-section .cities");
@@ -7,3 +7,20 @@ localStorage.setItem(
   "tokenKey",
   "APmZLfxtMtsZU2trsJj32PEnVbHzYqNQLpKk76K4PGiZ4xFV2VieIJjIg79JYHA4"
 );
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  getWeatherDataFromApi();
+});
+
+const getWeatherDataFromApi = async () => {
+  //   alert("http request is gone!");
+  const tokenKey = DecryptStringAES(localStorage.getItem("tokenKey"));
+  //   alert(tokenKey);
+  const inputValue = input.value;
+  const units = "metric";
+  const lang = "tr";
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&appid=${tokenKey}&units=${units}&lang=${lang}`;
+  const response = await fetch(url).then((response) => response.json());
+  console.log(response);
+};
