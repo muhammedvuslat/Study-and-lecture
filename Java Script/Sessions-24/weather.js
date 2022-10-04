3b47150512ea67f275460a926bddd86ad50aa363
muhammedvuslat <v.sltcevikev@gmail.com>
2022-10-04 15:39:23 +0300

    Weather-city-search-1

--- Java Script/Sessions-24/weather.js
+++ Java Script/Sessions-24/weather.js
@@ -23,4 +23,30 @@ const getWeatherDataFromApi = async () => {
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&appid=${tokenKey}&units=${units}&lang=${lang}`;
   const response = await fetch(url).then((response) => response.json());
   console.log(response);
+  const { main, sys, weather, name } = response;
+  const iconUrl = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${weather[0].icon}.svg`;
+  const cityNameSpans = list.querySelectorAll(".city span");
+  const cityNameSpansArray = Array.from(cityNameSpans);
+  if (cityNameSpansArray.length > 0) {
+  }
+  console.log(cityNameSpansArray);
+  const createdLi = document.createElement("li");
+  createdLi.classList.add("city");
+  createdLi.innerHTML = `
+  <h2 class="city-name" data-name="${name}, ${sys.country}">
+                             <span>${name}</span>
+                             <sup>${sys.country}</sup>
+                        </h2>
+                            <div class="city-temp">${Math.round(
+                              main.temp
+                            )}<sup>°C</sup></div>
+                            <figure>
+                                <img class="city-icon" src="${iconUrl}">
+                                <figcaption>${
+                                  weather[0].description
+                                }</figcaption>
+                            </figure>`;
+  //! append vs prepend
+  list.prepend(createdLi);
+  form.reset();
 };
