c3d72b7b6f90fe5600e12b486cc97e51d8be820d
muhammedvuslat <v.sltcevikev@gmail.com>
2022-10-14 09:07:54 +0300

    vers.-2

--- React/Sessions-02/rc02_jsx_comp_basicStyling/src/App.js
+++ React/Sessions-02/rc02_jsx_comp_basicStyling/src/App.js
@@ -1,4 +1,12 @@
 import React from "react";
+//! Componentler, HTML,CSS ve JS kodlarindan meydana gelmiş
+//! bir kod parcacigidir.
+//! Bir componentin return () kismi ise JSX kodlari icerir.
+//! JSX'in kendine gore bazi kurallari vardir. Ornegin bir
+//! component eger birden fazla html elementi dondurmesi
+//! gerekirse bunlari tek bir container elemani ile sarmayalarak
+//! dondurmelidir.container icin div, article, header, <> v.b
+//! herhangi bir element kullanilabilir.
 
 function App() {
   return <h1>JSX & COMPONENTS & BASİC STYLING</h1>;
--- React/Sessions-02/rc02_jsx_comp_basicStyling/src/index.js
+++ React/Sessions-02/rc02_jsx_comp_basicStyling/src/index.js
@@ -1,8 +1,51 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import App from "./App";
+import App from "./App"; //! App js importu
 import "./index.css";
+//! index componenti ReactDOM.createRoot() metodu ile
+//! public klasorunde yer alan index.html icersindeki id'si root
+//! olan elemana erisir.
+//* render() metodu içerisinde App componenti cagrilir.
+
+//?--------⬇--------
+//! 1. yorumdaki alan
+//?--------⬆--------
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(<App />); //! App js renderı
+//?--------⬇--------
+//! 2. yorumdaki alan
+//?--------⬆--------
+
+//?--------⬇--------
+
+// // ! JSX yapısı
+// // const element = (
+// //   <>
+// //     <h1>Reatc Start</h1>
+// //     <p>Hello </p>
+// //   </>
+// // );
+// //! JSX yapısı
+// //! JSX - Compenent yapısı
+// const App = () => {
+//   //! Compenent olarak kullanılması için elementin buyuk harfle başlaması gerekir
+//   return (
+//     <>
+//       <h1>Reatc Start Compenent</h1>
+//       <p>Hello </p>
+//       <Child /> {/*fonk. dısındaki Compenenti çagırırken*/}
+//       <Child /> {/*1 kere tanımla istediğin kadar kullan*/}
+//     </>
+//   );
+// };
+// const Child = () => <p> I am a child</p>;
+//! JSX - Compenent yapısı
+//?--------⬆--------
+
+//?--------⬇--------
+//! 2. yorumdaki alan
+// root.render(element); //! element jSX renderı
+// root.render(<App />); //! element jSX - Compenent renderı /baş harfi buyuk olduğunda Compenent /
+//?--------⬆--------
