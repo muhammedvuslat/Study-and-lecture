4e2bf289009e27b93b32b78b6defef48319a7d0f
muhammedvuslat <v.sltcevikev@gmail.com>
2022-10-17 22:19:31 +0300

    v.2

--- React/Sessions-04/solition/src/components/Main/Main.jsx
+++ React/Sessions-04/solition/src/components/Main/Main.jsx
@@ -1,17 +1,12 @@
 import "./Main.scss";
 import { data } from "../../helper/data";
+import Card from "./Card";
 const Main = () => {
-  console.log(data);
   return (
     <div className="card-container">
-      {data.map((item) => {
-        return (
-          <div className="cards" key={item.id}>
-            <h1>{item.title}</h1>
-            <img src={item.image} alt="img" />
-          </div>
-        );
-      })}
+      {data.map((item, index) => (
+        <Card {...item} key={index} />
+      ))}
     </div>
   );
 };
