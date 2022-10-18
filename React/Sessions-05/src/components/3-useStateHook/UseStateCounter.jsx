c2cc337e3477555af7127e31c51536254ec3b632
muhammedvuslat <v.sltcevikev@gmail.com>
2022-10-18 11:26:55 +0300

    v.4(UseStateObject)

--- React/Sessions-05/src/components/3-useStateHook/UseStateCounter.jsx
+++ React/Sessions-05/src/components/3-useStateHook/UseStateCounter.jsx
@@ -4,8 +4,6 @@
 //! React 16.8 versiyonu ile gelmistir ve geldikten sonra Class-componentler'in
 //! kullanimi cok azaltmistir.
 
-import { useState } from "react";
-
 //? React'ta useState(), useEffect(), useContext() gibi bir cok built-in
 //? Hook bulunmaktadir. Ayrica custom hook tanimlamak da mumkundur.
 
@@ -20,8 +18,75 @@ import { useState } from "react";
 //?    https://reactjs.org/docs/hooks-rules.html
 //* =============================================================
 
-const USeStateCounter = () => {
-  return <div>UseStateCounter</div>;
+import { useState } from "react";
+
+const UseStateCounter = () => {
+  //* useState en cok kullanilan Hook'tur.
+  //* Bir state'in degisken, dizi ve obje ile kullanilabilmesine olanak saglar.
+  //? useState hook'u bir dizi dondurur.Bu dizi array dest ile acilabilir.
+  //?  Acilan dizinin 1.elemani state degiskenidir.
+  //?  2.si ise state'i degistirmeye izin veren bir setter metodudur.
+  //? useState parametre olarak state'in ilk degerini alir.
+  const [count, setCount] = useState(0); //! array destructuring
+
+  //! ⬇Obje örneği için
+  const [person, setPerson] = useState({
+    name: "John",
+    surname: "Doe",
+    age: 43,
+  });
+  //! ⬆
+  const inc = () => {
+    setCount(count + 1);
+  };
+  const dec = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  };
+  const incAge = () => {
+    //! Bu sekilde bir atama ile sayisal deger state'in uzerine yazilmis oldu.
+    //! Dolayisiyla obje yapisi bozuldu.
+    //? setPerson(person.age + 1);
+    //? setPerson({ name: "Ahmet", surname: "Can", age: 44 });
+    setPerson({ ...person, age: person.age + 1 });
+  };
+  return (
+    <div className="container text-center mt-4">
+      <section>
+        <h1>USESTATE HOOK</h1>
+        <h2 className="displat-4 text-danger">COUNT {count} </h2>
+        <button onClick={inc} className="btn btn-success">
+          INC
+        </button>
+        <button onClick={() => setCount(0)} className="btn btn-warning">
+          Clear{" "}
+          {/* //!setCount(0) a eşitler fakat başına fonk. yazılmaz ise 
+        //! tıklndıında değil sürekli çalıştırır o yüzden inline fonk. ()=> 
+        //! setCount(0) tercih edilir*/}
+        </button>
+        <button onClick={dec} className="btn btn-danger">
+          DEC
+        </button>
+        {/* <button
+        onClick={() => count > 0 && setCount(0)}
+        className="btn btn-danger"
+      >
+        DEC
+      </button> //! 0 dan küçük ise eksiye inmemesi için yazılan fonk. inline metodu */}
+      </section>
+      <section>
+        <h1>USESTATE OBJECT</h1>
+        <h2>{person.name}</h2>
+        <h2>{person.surname}</h2>
+        <h4>{person.age}</h4>
+        <button onClick={incAge} className="btn btn-info">
+          {" "}
+          İnc Age
+        </button>
+      </section>
+    </div>
+  );
 };
 
 export default UseStateCounter;
