4fe714643011a9a33f4b4ae2f1d6e3c66af94d9b
muhammedvuslat <v.sltcevikev@gmail.com>
2022-10-22 07:54:43 +0300

    Hover double click controls

--- React/Sessions-06/src/components/2-keyboardClipboardEvents/KeyboardClipboard.jsx
+++ React/Sessions-06/src/components/2-keyboardClipboardEvents/KeyboardClipboard.jsx
@@ -11,6 +11,12 @@ const KeyboardClipboard = () => {
       alert("Please dont enter a number");
       e.preventDefault();
     }
+    e.keyCode === 13 && setInputValue("");
+  };
+  const handleAreaPaste = (e) => {
+    e.target.style.border = "2px solid red";
+    e.target.value += e.clipboardData.getData("text").toLocaleUpperCase();
+    e.preventDefault();
   };
   return (
     <div className="container text-center">
@@ -18,15 +24,30 @@ const KeyboardClipboard = () => {
       <input
         className="form-control"
         type="text"
+        value={inputValue}
         onChange={(e) => {
-          e.target.value = e.target.value.toLocaleUpperCase(); //! İnputa sadece Büyük harf ile girilmesini sağlar
+          e.target.value = e.target.value.toLocaleUpperCase(); //! İnput'a sadece Büyük harf ile girilmesini sağlar
           setInputValue(e.target.value);
         }}
         onKeyDown={handleKeyDown}
       />
-      <p className="text-start mt-4">{inputValue}</p>
+      <p
+        onCopy={(e) => {
+          e.preventDefault();
+          alert("You cannot copy this doc");
+        }}
+        className="text-start mt-4"
+      >
+        {inputValue}
+      </p>
+      <textarea
+        className="form-control"
+        name="area"
+        id="area"
+        rows="10"
+        onPaste={handleAreaPaste}
+      ></textarea>
     </div>
   );
 };
-
 export default KeyboardClipboard;
--- React/Sessions-06/src/components/3-mouseEvents/MouseEvent.jsx
+++ React/Sessions-06/src/components/3-mouseEvents/MouseEvent.jsx
@@ -1,22 +1,55 @@
+import { useState } from "react";
 const MouseEvent = () => {
+  const [showHint, setShowHint] = useState(false);
+  const [toggle, setToggle] = useState(false);
+  const [coordX, setCoordX] = useState();
+  const [coordY, setCoordY] = useState();
+  const handleDoubleClick = (e) => {
+    setToggle(!toggle);
+    toggle
+      ? (e.target.className = "bg-success text-light w-50 p-4 mt-4")
+      : (e.target.className = "bg-warning text-dark w-50 p-4 mt-4");
+  };
+  console.log(toggle);
+  //? mouseMove event
+  const handleMouseMove = (e) => {
+    // setCoordX(e.nativeEvent.offsetX); //! div içerisindeki konuma göre
+    // setCoordY(e.nativeEvent.offsetY); //! div içerisindeki konuma göre
+    setCoordX(e.pageX); //! sayfa içerisindeki konuma göre
+    setCoordY(e.pageY); //! sayfa içerisindeki konuma göre
+  };
   return (
     <div className="container text-center d-flex flex-column align-items-center mt-4">
       <h2 className="text-danger">MOUSE EVENTS</h2>
 
-      <div id="todo-1" className="bg-success text-light w-50 p-4">
-        todo item 1
+      <div
+        id="todo-1"
+        className="bg-success text-light w-50 p-4"
+        onMouseEnter={() => setShowHint(true)}
+        onMouseOut={() => setShowHint(false)}
+      >
+        Hover with mouse
+      </div>
+      {showHint && <p>Hello React</p>}
+      <div
+        id="todo-2"
+        className="bg-success text-light w-50 p-4 mt-4"
+        onDoubleClick={handleDoubleClick}
+      >
+        Double click with mouse
       </div>
 
-      <div id="todo-2" className="bg-success text-light w-50 p-4 mt-4">
-        todo item 2
+      <div
+        id="todo-3"
+        className="bg-success text-light w-50 p-4 my-4"
+        onMouseMove={handleMouseMove}
+      >
+        Hover with mouse
       </div>
 
-      <div id="todo-3" className="bg-success text-light w-50 p-4 my-4">
-        todo item 3
-      </div>
-
-      <p>X and Y</p>
-      <p className="text-danger fw-bold">coordX - coordY</p>
+      <p className="text-danger fw-bold">
+        X:{coordX} Y:{coordY}
+      </p>
     </div>
   );
 };
