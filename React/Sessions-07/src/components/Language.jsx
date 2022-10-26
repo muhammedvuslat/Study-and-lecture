b229d0900f6cf2b05accddd36640165b5cb3c4e3
muhammedvuslat <v.sltcevikev@gmail.com>
2022-10-26 20:00:26 +0300

    end

--- React/Sessions-07/src/components/Language.jsx
+++ React/Sessions-07/src/components/Language.jsx
@@ -1,13 +1,30 @@
+import { useState } from "react";
 import Container from "react-bootstrap/Container";
 import Image from "react-bootstrap/Image";
 const Language = ({ name, img, options }) => {
-  console.log(name, img, options);
+  const [showImage, setShowImage] = useState(true);
+  // console.log(name, img, options);
+  console.log(showImage);
   return (
-    <Container className="p-3 rounded-2" style={{ background: "peachpuff" }}>
-      <Container>
-        <Image src={img} width="70%"></Image>
-        <h3 className="display-6">{name}</h3>
-      </Container>
+    <Container
+      className="p-3 rounded-2 h-100 lang-card"
+      style={{ background: "peachpuff" }}
+      onClick={() => setShowImage(!showImage)}
+      type="button"
+    >
+      {showImage && (
+        <Container>
+          <Image className="lang-logo" src={img} width="70%"></Image>
+          <h3 className="display-6 ">{name}</h3>
+        </Container>
+      )}
+      {!showImage && (
+        <ol className="h-100 d-flex flex-column justify-content-center">
+          {options.map((item) => {
+            return <li className="text-start h5">{item}</li>;
+          })}
+        </ol>
+      )}
     </Container>
   );
 };
