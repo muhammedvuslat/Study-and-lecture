9a916f76561a577954511bfd9efc312d5d466447
muhammedvuslat <v.sltcevikev@gmail.com>
2022-10-29 12:52:16 +0300

    show-hide_button

---
+++ React/Sessions-08/src/App.js
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import LifeCycleMethods from "./components/LifeCycleMethods";
+
+function App() {
+  const [show, setShow] = useState(true);
+  return (
+    <div className="container text-center mt-4">
+      <button className="btn btn-dark" onClick={() => setShow(!show)}>
+        {show ? "Hide" : "Show"}
+      </button>
+      {show && <LifeCycleMethods />}
+    </div>
+  );
+}
+
+export default App;
