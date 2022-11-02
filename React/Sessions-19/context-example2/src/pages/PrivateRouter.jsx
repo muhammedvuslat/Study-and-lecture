94846041cbae4c9ee0b5d35d0523a9dea3d7f3c7
muhammedvuslat <v.sltcevikev@gmail.com>
2022-11-02 22:47:20 +0300

    Start

--- React/Sessions-19/context-example2/src/pages/PrivateRouter.jsx
+++ React/Sessions-19/context-example2/src/pages/PrivateRouter.jsx
@@ -1,12 +1,13 @@
 import { useContext } from "react";
 import { Outlet, Navigate } from "react-router-dom";
-import { LoginContext } from "../context/LoginContext";
+import { LoginContext, useLoginContext } from "../context/LoginProvider";
 
 const PrivateRouter = () => {
   // const user = true;
 
   //! Consuming
-  const { user } = useContext(LoginContext);
+  // const { user } = useContext(LoginContext);
+  const { user } = useLoginContext();
 
   return user?.email ? <Outlet /> : <Navigate to="/login" />;
 };
