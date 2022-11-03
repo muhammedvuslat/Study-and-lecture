6a858a04f3558eebb54e9563aee4e2e22190ad63
muhammedvuslat <v.sltcevikev@gmail.com>
2022-11-03 23:07:35 +0300

    color-palette experiments

--- React/Sessions-20/material-intro-inClass/src/pages/Home.jsx
+++ React/Sessions-20/material-intro-inClass/src/pages/Home.jsx
@@ -1,10 +1,29 @@
+import CardGrid from "../components/CardGrid";
+import TextFieldComp from "../components/TextFieldComp";
 import TypoButtons from "../components/TypoButtons";
+import AppbarComp from "../components/AppbarComp";
+import { createTheme, ThemeProvider } from "@mui/material";
+import { teal } from "@mui/material/colors";
 
 const Home = () => {
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: "#21a415",
+      },
+      secondary: {
+        main: teal[200],
+      },
+    },
+  });
+
   return (
-    <>
+    <ThemeProvider theme={theme}>
+      <AppbarComp />
       <TypoButtons />
-    </>
+      <TextFieldComp />
+      <CardGrid />
+    </ThemeProvider>
   );
 };
 
