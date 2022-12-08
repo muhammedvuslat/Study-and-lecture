926ef4b1d8edf7cb27766fb955348ed4f93149f0
muhammedvuslat <v.sltcevikev@gmail.com>
2022-12-08 08:39:15 +0300

    v.3.1(card-grid-style)

--- React/Sessions-17/src/components/CardGrid.jsx
+++ React/Sessions-17/src/components/CardGrid.jsx
@@ -5,33 +5,43 @@ import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import { Button, CardActionArea, CardActions } from "@mui/material";
 import data from "../data";
+import Grid from "@mui/material/Grid";
 
 const CardGrid = () => {
   return (
     <>
-      {data.map((cards) => {
-        const { id, name, text, img } = cards;
-        return (
-          <Card sx={{ maxWidth: 345, mt: 3 }}>
-            <CardActionArea>
-              <CardMedia component="img" image={img} alt="img" />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                  {name}
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  {text}
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-            <CardActions>
-              <Button size="small" color="primary">
-                Detail
-              </Button>
-            </CardActions>
-          </Card>
-        );
-      })}
+      <Typography variant="h4" color="error" align="center" mt={4}>
+        Card & Grid
+      </Typography>
+      <Grid container spacing={2} justifyContent="center" alignItems="center">
+        {data.map((cards) => {
+          const { id, name, text, img } = cards;
+          return (
+            <>
+              <Grid item xs={12} sm={6} md={4}>
+                <Card sx={{ mt: 3 }} key={id}>
+                  <CardActionArea>
+                    <CardMedia component="img" image={img} alt="img" />
+                    <CardContent>
+                      <Typography gutterBottom variant="h5" component="div">
+                        {name}
+                      </Typography>
+                      <Typography variant="body2" color="text.secondary">
+                        {text}
+                      </Typography>
+                    </CardContent>
+                  </CardActionArea>
+                  <CardActions>
+                    <Button size="small" color="primary">
+                      Detail
+                    </Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            </>
+          );
+        })}
+      </Grid>
     </>
   );
 };
