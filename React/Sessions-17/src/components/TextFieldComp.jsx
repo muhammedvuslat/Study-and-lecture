8c2074949febf6d3ece05f2c159ee601423de8a7
muhammedvuslat <v.sltcevikev@gmail.com>
2022-12-08 07:47:39 +0300

    v.2(text-field)

--- React/Sessions-17/src/components/TextFieldComp.jsx
+++ React/Sessions-17/src/components/TextFieldComp.jsx
@@ -3,16 +3,36 @@ import Container from "@mui/material/Container";
 import Button from "@mui/material/Button";
 import Box from "@mui/system/Box";
 import TextField from "@mui/material/TextField";
+import { useState } from "react";
 
 const TextFieldComp = () => {
+  const [error, setError] = useState(false);
   return (
     <div>
       <Container>
         <Typography variant="h4" color="error" align="center" mt={4}>
           Text Field
         </Typography>
-        <Box>
-          <TextField />
+        <Box sx={{ mt: 4 }}>
+          <TextField
+            // margin="dense" margin="normal" gibi  MUI nun özel marginide kullanılabilinir
+            id="email"
+            label="Email"
+            pleaceholde="Enter your email"
+            fullWidth
+            error={error}
+            helperText={error && "Incorrect email format"}
+          />
+
+          <TextField
+            id="password"
+            label="Password"
+            pleaceholde="Enter your password"
+            fullWidth
+            error={error}
+            helperText={error && "Incorrect  password"}
+            sx={{ mt: 2 }}
+          />
         </Box>
       </Container>
     </div>
