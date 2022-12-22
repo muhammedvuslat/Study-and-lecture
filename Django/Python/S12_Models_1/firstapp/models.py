a130bbfcfdcea0dbe824e5608c7cce92999a8956
muhammedvuslat <v.sltcevikev@gmail.com>
2022-12-22 21:40:31 +0300

    createsuperuser-admin_panel

--- Django/Python/S12_Models_1/firstapp/models.py
+++ Django/Python/S12_Models_1/firstapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    number = models.IntegerField()
+    about = models.TextField(blank=True, null=True)
+    register = models.DateTimeField(auto_now_add=True) # yaratılış tarih ve saatini 
+    last_updated_date = models.DateTimeField(auto_now=True) # güncelleme tarih ve saatini 
+    is_activate = models.BooleanField()
