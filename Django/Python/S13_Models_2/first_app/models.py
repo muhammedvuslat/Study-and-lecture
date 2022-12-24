817952e1f816e789f7a4a6a41b73e645bf1d45e2
muhammedvuslat <v.sltcevikev@gmail.com>
2022-12-24 23:32:24 +0300

    New_app-relation

--- Django/Python/S13_Models_2/first_app/models.py
+++ Django/Python/S13_Models_2/first_app/models.py
@@ -6,6 +6,16 @@ class Student(models.Model):
     last_name = models.CharField(max_length=30)
     number = models.SmallIntegerField(blank=True)
     is_activate = models.BooleanField(blank=True, null=True)
+    email = models.EmailField(blank=True)
+    about = models.TextField(blank=True)
+    avatar = models.ImageField(blank=True, null=True, upload_to='student') #Kaydolan media verilerinin student klasörü altında toplanmasını sağlar
+    register_date = models.DateTimeField(auto_now_add=True)
+    files = models.FileField(blank=True, null=True, upload_to='student_files')
+    update_date = models.DateTimeField(auto_now=True)
+
 
     def __str__(self):
         return f" Number:{self.number} Name: {self.first_name} LastName: {self.last_name}"
+    class Meta:
+        ordering = ['number']
+    
