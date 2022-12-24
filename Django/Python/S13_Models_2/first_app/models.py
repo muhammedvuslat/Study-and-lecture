2c27eb4f5592826c85c137037cdb1143ebac6b1f
muhammedvuslat <v.sltcevikev@gmail.com>
2022-12-24 12:28:07 +0300

    delete

--- Django/Python/S13_Models_2/first_app/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Student(models.model):
-    first_name = models.CharField(max_length=30, blank=True)
-    last_name = models.CharField(max_length=40)
-    number = models.PositiveSmallIntegerField(blank=True)
