d9896a0ff3125d6b75fa40f5296529cc11cc4e46
muhammedvuslat <v.sltcevikev@gmail.com>
2022-12-24 12:24:27 +0300

    first_app_start-model.py

--- Django/Python/S13_Models_2/first_app/models.py
+++ Django/Python/S13_Models_2/first_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.model):
+    first_name = models.CharField(max_length=30, blank=True)
+    last_name = models.CharField(max_length=40)
+    number = models.PositiveSmallIntegerField(blank=True)
