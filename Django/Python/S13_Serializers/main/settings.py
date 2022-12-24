c7916136042c8b7d688085a574f1d25c5863a06f
muhammedvuslat <v.sltcevikev@gmail.com>
2022-12-25 02:13:52 +0300

    project-secret_key-decouple_config_setting(v2)

--- Django/Python/S13_Serializers/main/settings.py
+++ Django/Python/S13_Serializers/main/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^r4+m2ratwacr$wn26hu^2^v+57-sru&#2hmtz0&g!mpd41*#s'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
