a1544d23ac9d6d87d47f448ef3e1cbdd7682ee8e
muhammedvuslat <v.sltcevikev@gmail.com>
2022-12-25 01:15:54 +0300

    Work_project-finish

--- Django/Python/S13_Models_2/relation/admin.py
+++ Django/Python/S13_Models_2/relation/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import (Adress,Product,Profile)
 # Register your models here.
+admin.site.register(Adress)
+admin.site.register(Profile)
+admin.site.register(Product)
--- Django/Python/S13_Models_2/relation/models.py
+++ Django/Python/S13_Models_2/relation/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    bio = models.TextField(blank=True)
+    image = models.ImageField(upload_to='profile', blank=True, null=True )
+    user = models.OneToOneField(User, on_delete=models.CASCADE) # CASCADE user silindiğinde usera ayit profile ı siler
+    def __str__(self) :
+        return self.user.username
+
+'''
+on_delete properties:
+    # CASCADE -> if primary deleted, delete foreing too.
+    # SET_NULL -> if primary deleted, set foreign to NULL. (null=True)
+    # SET_DEFAULT -> if primary deleted, set foreing to DEFAULT value. (default='Value')
+    # DO_NOTHING -> if primary deleted, do nothing.
+    # PROTECT -> if foreign is exist, can not delete primary.
+'''
+
+class Adress(models.Model):
+    name = models.CharField(max_length=20)
+    adress = models.CharField(max_length=150)
+    city = models.CharField(max_length=20)
+    phone = models.CharField(max_length=20)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # birden fazla user adına kayıt yapılabilinir 
+
+
+    def __str__(self):
+        return f"{self.name} {self.user.username}"
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    user = models.ManyToManyField(User) # bir ürün birden fazla useraaa kayıt olabilir 
+
+    def __str__(self):
+        return self.name
