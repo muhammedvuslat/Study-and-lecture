b68e7b48bc4829404922babb5f165fca7250e3ee
muhammedvuslat <v.sltcevikev@gmail.com>
2022-12-26 03:27:47 +0300

    Serializer(v6)

--- Django/Python/S13_Serializers/student_api/models.py
+++ Django/Python/S13_Serializers/student_api/models.py
@@ -1,10 +1,19 @@
 from django.db import models
 
+class Path(models.Model):
+    path_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.path_name}"
+
 class Student(models.Model):
+    path = models.ForeignKey(Path, related_name='students', on_delete=models.CASCADE)
+    # Bu noktadaki path üst class
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     number = models.IntegerField(blank=True, null=True)
     age = models.IntegerField()
 
     def __str__(self):
-        return self.first_name
+        return f"{self.first_name} {self.last_name}"
+
--- Django/Python/S13_Serializers/student_api/serializers.py
+++ Django/Python/S13_Serializers/student_api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Student
+from .models import Student, Path
 
 #! Klasik-Temel serializer yöntemi 
 '''class StudentSerializer(serializers.Serializer):
@@ -28,10 +28,16 @@ from .models import Student
 class StudentSerializer(serializers.ModelSerializer):
 
     born_year = serializers.SerializerMethodField()
+    # SerializerMethodField read_only dir yani sadece okumaya yöneliktir post yaparken boşta olsa yazılmamalı.
     # SerializerMethodField methodu bizden bir etod beklemekte ve alt satırlarda get_born_year ile yeni bir verimizi database işlemeden front ende göndermiş olduk. aynı zamanda Meta clasında bu eklenen datayı bbelirtmemmiz gerekli
+    path= serializers.StringRelatedField()
+    # aynı zamanda isimlerinde uyuşması gerekli (models da ki tablo classı isimleri ile)
+    #! Aynı şekilde read_only
+    #! StringRelatedField, modelde gösterilen __str__ yöntemini kullanarak ilişkinin hedefini temsil etmek için kullanılabilir. <https://www.django-rest-framework.org/api-guide/relations/>
+    path_id = serializers.IntegerField()
     class Meta:
         model = Student       
-        fields = ["id","first_name", "last_name","number", "age", "born_year"]
+        fields = ["id","first_name", "last_name","number", "age", "born_year" , "path","path_id"]
 
         #! Fields ları filtremek istediğimizde şu şekilde haraket ederiz
         #fields = ["last_name" , "first_name"]
@@ -41,5 +47,34 @@ class StudentSerializer(serializers.ModelSerializer):
         import datetime
         current_time = datetime.datetime.now() #metodla şimdi ki zamanı aldık
         return current_time.year - obj.age # get işlemi olarak tablodaki age i şimdi ki zamandan çıkartıp doğum yılına ulaştık
-      
+class PathSerializer(serializers.ModelSerializer):
+
+    #students = StudentSerializer(many=True)
+    #! Studenet tablosunu pathın altında çektiğimizde api dan bu şekilde bir çıktı alınır
+    '''{
+        "id": 1,
+        "path_name": "fs",
+        "students": [
+            {
+                "id": 2,
+                "first_name": "Ahmet",
+                "last_name": "Mehmet",
+                "number": 2,
+                "age": 33,
+                "born_year": 1989,
+                "path": "fs",
+                "path_id": 1
+            }
+        ]
+    }''' 
+    students = serializers.HyperlinkedRelatedField(
+        many=True,
+        read_only=True,
+        view_name='detail'
+    )
+   
     
+    class Meta:
+        model = Path
+        # fields = "__all__"
+        fields = ["id", "path_name", "students"]
