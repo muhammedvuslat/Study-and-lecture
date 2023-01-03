939acfed20c4a3d65afd6e86bb100cc82216bfc8
muhammedvuslat <v.sltcevikev@gmail.com>
2023-01-03 22:46:12 +0300

    Generic_API_View(v3)

--- Django/Python/S16_Class_based_views/student_api/views.py
+++ Django/Python/S16_Class_based_views/student_api/views.py
@@ -5,6 +5,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from django.shortcuts import render, HttpResponse, get_object_or_404
 from rest_framework.views import APIView
+from rest_framework.generics import GenericAPIView, mixins
 
 #! My import
 from .models import Student, Path
@@ -177,7 +178,25 @@ class StudentDetail(APIView):
         }
         return Response(data)
 
-    
+#! GENERİCAPI View
 
+class StudentGAV(mixins.ListModelMixin,mixins.CreateModelMixin, GenericAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
 
+    def list(self,request,*args,**kwargs):
+        return self.list(request,*args,**kwargs)
+    def create(self,request,*args, **kwargs):
+        return self.create(request,*args, **kwargs)
+
+class StudentDetailGAV(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, GenericAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+    def retrieve(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+    def update(self, request, *args, **kwargs):
+         return self.update(request, *args, **kwargs)
+    def destroy(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
   
