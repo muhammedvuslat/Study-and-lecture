70fe5148a7f8c86858ab00532a5addda7f463f0b
muhammedvuslat <v.sltcevikev@gmail.com>
2023-01-04 02:23:02 +0300

    models_created(v2)

--- Django/Python/S17_Task_Tracker/todo/models.py
+++ Django/Python/S17_Task_Tracker/todo/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Todo(models.Model):
+    # Front-end'e  Sınırlı seçenek sunmak için
+    PRIORITY = (
+        (1, 'High'),
+        (2, 'Medium'),
+        (3, 'Low')
+    )
+    task = models.CharField(max_length=50) 
+    description = models.TextField(blank=True) #? vlank zorunlu olmayan
+    priority = models.SmallIntegerField(choices=PRIORITY,default=3) #? Choices Otomatik olarak seçimleri gelmesi
+    is_done = models.BooleanField(default=False) #? Yeni yaratılan bir task bitmiş olamaz başlangıcta false olması sebebi
+    updatea_date = models.DateTimeField(auto_now=True) #? Update işlemlerinin tarih-saatinin alana kaydeder
+    create_date = models.DateTimeField(auto_now_add=True) #? Ekleme oluşturma işleminde tarih-saat ekleyecektir
+
+    def __str__(self) :
+        return self.task
