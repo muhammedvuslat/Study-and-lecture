cfad6c0b90369369933cf062d9744285746678dc
muhammedvuslat <v.sltcevikev@gmail.com>
2023-01-04 09:15:42 +0300

    serializers_views-created(v3)

--- Django/Python/S17_Task_Tracker/todo/models.py
+++ Django/Python/S17_Task_Tracker/todo/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Todo(models.Model):
     # Front-end'e  Sınırlı seçenek sunmak için
     PRIORITY = (
-        (1, 'High'),
+        (1, 'High'), #! 1 back-end , High Front-end
         (2, 'Medium'),
         (3, 'Low')
     )
@@ -12,7 +12,7 @@ class Todo(models.Model):
     priority = models.SmallIntegerField(choices=PRIORITY,default=3) #? Choices Otomatik olarak seçimleri gelmesi
     is_done = models.BooleanField(default=False) #? Yeni yaratılan bir task bitmiş olamaz başlangıcta false olması sebebi
     updatea_date = models.DateTimeField(auto_now=True) #? Update işlemlerinin tarih-saatinin alana kaydeder
-    create_date = models.DateTimeField(auto_now_add=True) #? Ekleme oluşturma işleminde tarih-saat ekleyecektir
+    created_date = models.DateTimeField(auto_now_add=True) #? Ekleme oluşturma işleminde tarih-saat ekleyecektir
 
     def __str__(self) :
         return self.task
--- Django/Python/S17_Task_Tracker/todo/views.py
+++ Django/Python/S17_Task_Tracker/todo/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+#! My Framework import
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+#! My import
+from .models import Todo
+from .serializers import TodoSerializer
+
+@api_view(['GET','POST'])
+def todo_list_create(request):
+    if request.method == 'GET':
+        todos = Todo.objects.filter(is_done=False) #! Get işlemi normalde objects.all() şeklinde yapılır fakat biz tamamlanmayanları çekmek istediğim için is_done=False olarak işlem yaptık
+        serializer = TodoSerializer(todos, many=True) #! queryset olarak gelen data yapısı serializer değişkenine TodoSerializer aracılığıyla alınarak json veri tipine döner. many=True birden fazla queryset olacağı için yazılır
+        return Response(serializer.data)
