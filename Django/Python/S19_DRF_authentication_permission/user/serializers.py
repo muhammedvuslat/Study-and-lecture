d201644dfdcefc61180a27f1182cb340398b6dd8
muhammedvuslat <v.sltcevikev@gmail.com>
2023-01-08 14:43:14 +0300

    serializer_crate-views_import(v.3.3)

--- Django/Python/S19_DRF_authentication_permission/user/serializers.py
+++ Django/Python/S19_DRF_authentication_permission/user/serializers.py
@@ -5,7 +5,7 @@ from rest_framework.validators import UniqueValidator #! Unique validate import 
 class RegisteSerializer(serializers.ModelSerializer):
     email = serializers.EmailField(required=True, validators = [UniqueValidator(queryset=User.objects.all())] ) #! User kayıt işleminde default olan email zorunlu olmama özelliğini required=True ile zorunlu hale getiririyoruz|| Emaillerin unique olması için DRF unique validator paketini kullancağız|| Bu alandaki user import ettiğimiz User tablosunun bütün objelerinin unique olması şartını sağlar.
     password = serializers.CharField(write_only=True) #! write_only=True sadece yazma işlmeinde password kullanmamızı sağlar, get işlemlerinde passwor istemez.|| bir diğeri ise read_only=True sadece okuma yani get işlemleriende passwordü çalıştırır.
-    password2 = serializers.CharField(write_only=True,required=True)
+    password2 = serializers.CharField(write_only=True,required=True) #! Models de data olarak tutulan password ile front end de girilen password 2 yi karşılaştıracağız
 
     class Meta:
         model = User
@@ -18,3 +18,17 @@ class RegisteSerializer(serializers.ModelSerializer):
             'password',
             'password2'
         )
+    def validate(self, data):
+        if data['password'] != data['password2']:
+            raise serializers.ValidationError( #! password ile password2 karşılaştırıldı aynı değillerse hata mesajı belirtildi 
+                {'password':'Password fields didnt match.'}
+            ) 
+        return data 
+
+    def create(self, validated_data):
+        validated_data.pop('password2') #! passwordd2 yi kullanıcı yaratırken ederken  database e göndermeyeceğimiz için sildik
+        password = validated_data.pop('password') #! passwordü de şifreleyip database aktaracağımız için silip password değişkenine atadık
+        user = User.objects.create(**validated_data) #! User yaratırken fields lara ulaşmak için ** kullandık bu şu anlama gelir username=validate_data['username'], email=validate_data['email']...... normal yazmış olsaydık bu şekilde tek tek yazmamız gerekliydi
+        user.set_password(password) #! user a yeni passwordü şifrelenmiş şekilde kayıt ediyoruz
+        user.save() #! User kayıt işlemi
+        return user
--- Django/Python/S19_DRF_authentication_permission/user/views.py
+++ Django/Python/S19_DRF_authentication_permission/user/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from .serializers import RegisteSerializer
+from django.contrib.auth.models import User
+from rest_framework.generics import CreateAPIView
 
-# Create your views here.
+class RegisterView(CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisteSerializer
+
+
