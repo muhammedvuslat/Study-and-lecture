6e482dcb3bba989b8c54f9795aaba70ebbb61c61
Muhammed Vuslat ÇEVİK <105074236+muhammedvuslat@users.noreply.github.com>
2023-01-09 10:23:06 +0300

    notes changes

--- Django/Python/S19_DRF_authentication_permission/user/urls.py
+++ Django/Python/S19_DRF_authentication_permission/user/urls.py
@@ -4,6 +4,6 @@ from rest_framework.authtoken import views #! obtain_token için import
 
 urlpatterns = [
     path('register/', RegisterView.as_view()),
-    path('login/', views.obtain_auth_token) #! kullanıcı username ve password işlemlerini bu endpoint te post ettiğinde obtain_auth_token otomatik bir token dönecek ve bu token aynı zamanda database de kayıt olacak postman>post>http://127.0.0.1:8000/user/login/  post işlemini yaptığımızda admin panelde ki token tablosunda ilgili kullanıcı için token oluştuğunu görebiliriz.
+    path('login/', views.obtain_auth_token) #! kullanıcı username ve password işlemlerini bu endpoint te post ettiğinde obtain_auth_token otomatik bir token dönecek ve bu token aynı zamanda database de kayıt olacak | | | bu durumu farklı bir yoluda json fromatında yazarak user oluşturmak postman>post>http://127.0.0.1:8000/user/login/  post işlemini yaptığımızda admin panelde ki token tablosunda ilgili kullanıcı için token oluştuğunu görebiliriz.
 
 ]
