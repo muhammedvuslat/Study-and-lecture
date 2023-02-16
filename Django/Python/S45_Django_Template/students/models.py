e3167d4698503bfe2e0430ce200b6e1edb5b95c3
muhammedvuslat <v.sltcevikev@gmail.com>
2023-02-16 08:27:06 +0300

    models-created(v.5)

--- Django/Python/S45_Django_Template/students/models.py
+++ Django/Python/S45_Django_Template/students/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Students(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    number = models.SmallIntegerField()
+    image = models.ImageField(upload_to='students', blank=True)
+
+    def __str__(self):
+        return f'{self.number}{self.first_name}'
