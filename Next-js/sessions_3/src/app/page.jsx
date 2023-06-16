c74e8a353eeeef800db01fe04a0474a86bccee47
muhammedvuslat <v.sltcevikev@gmail.com>
2023-06-16 07:08:01 +0300

    Next.js-Style-Tailwind_css

--- Next-js/sessions_3/src/app/page.jsx
+++ Next-js/sessions_3/src/app/page.jsx
@@ -7,6 +7,7 @@ export default function Page() {
     <div className={styles.container}>
       <h1 className={styles["main-title"]}>Home Page Module stil</h1>
       <h1>Home Page Global stil</h1>
+      <p className="text-emerald-600">Page component test</p>
       <Test style={styles["body-title"]} />
       {/* Props ile Test componentine style gönderimi sağlanır ve test componenti içerisinde className'inde bildirilir */}
     </div>
